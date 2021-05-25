d0b9128e80e11f06441b975cf654114fa161d47d
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-05-24 18:34:49 -0700

    adds route for Find All end point for product API - confirms route works in postman

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('product')
 export class ProductController {
+  @Get()
   findAll(): string {
     return 'Find ALL';
   }
