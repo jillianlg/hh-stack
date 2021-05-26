90aac51bdd04930deceaffd7bb2d0936424f4994
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-05-26 10:53:59 -0700

    updates generic function to include a string, number and boolean, console log returns as expected

--- typescript-prctice/app.js
+++ typescript-prctice/app.js
@@ -2,4 +2,8 @@ function LoggerAndReturn(thing) {
     return thing;
 }
 var message = LoggerAndReturn('Hello World');
+var message2 = LoggerAndReturn(2);
+var message3 = LoggerAndReturn(true);
 console.log(message);
+console.log(message2);
+console.log(message3);
--- typescript-prctice/app.ts
+++ typescript-prctice/app.ts
@@ -3,5 +3,9 @@ function LoggerAndReturn<T>(thing: T) : T {
 }
 
 const message: string = LoggerAndReturn('Hello World');
+const message2: number = LoggerAndReturn(2);
+const message3: boolean = LoggerAndReturn(true);
 console.log(message);
+console.log(message2);
+console.log(message3);
 
