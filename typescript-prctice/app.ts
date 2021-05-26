130c7502b95fb4df8a39839eb93875cbebedd09f
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-05-26 10:50:32 -0700

    removes angle brackets from generic function, console log returns as expected

--- typescript-prctice/app.ts
+++ typescript-prctice/app.ts
@@ -2,6 +2,6 @@ function LoggerAndReturn<T>(thing: T) : T {
   return thing;
 }
 
-const message: string = LoggerAndReturn<string>('Hello World');
+const message: string = LoggerAndReturn('Hello World');
 console.log(message);
 
