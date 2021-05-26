66739edf1571f2e25cabef34615ecab90135cc81
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-05-26 10:35:38 -0700

    initiates js file by running tsc app.ts in the terminal

---
+++ typescript-prctice/app.js
@@ -0,0 +1,8 @@
+var HelloWorld = /** @class */ (function () {
+    function HelloWorld(message) {
+        console.log(message);
+    }
+    return HelloWorld;
+}());
+var helloWorld = new HelloWorld('Welcome to Typescript');
+console.log(helloWorld);
