986880116988208035a77383657a7452cd5ef32a
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-05-26 17:25:54 -0700

    adds debuging files

--- typescript-prctice/out/app.js
+++ typescript-prctice/out/app.js
@@ -4,6 +4,113 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+// Generic Function - T Array Type
+function getArray(items) {
+    return new Array().concat(items);
+}
+var myNumArr = getArray([100, 200, 300]);
+var myStrArr = getArray(['Hello', 'World']);
+var myBooArr = getArray([true, false]);
+// console.log(myNumArr);
+// console.log(myStrArr); 
+// console.log(myBooArr); 
+// Generic Function - Multiple Type Variables
+function getInfo(id, name) {
+    // console.log((typeof id + ', ' + typeof name));
+}
+getInfo(1, 'Jane'); // number, string
+// Generic Function - Non Generic Type Variables
+function displayType(id, name) {
+    // console.log(typeof id + ', ' + typeof name);
+}
+displayType(2, 'Malik'); // number, string
+// Generic Constraints
+var Customer = /** @class */ (function () {
+    function Customer(fname, lname) {
+        this.firstName = fname;
+        this.lastName = lname;
+    }
+    return Customer;
+}());
+// <T extends Customer> 
+// adds the constraint to limit the customerLogger to taking in only the Customer Type
+function customerLogger(customer) {
+    // console.log(`${customer.firstName} ${customer.lastName}`);
+}
+var customer = new Customer('Jane', 'Doe');
+customerLogger(customer);
+var p = { first: '10k', second: 1000 };
+// console.log(p);
+var person = { first: 'Natasha', second: 'Lutzski' };
+var newObj = {
+    id: Math.random().toString(36),
+    run: function () {
+        return 3;
+    }
+};
+function checkElementAt(items, toBeChecked, atIndex) {
+    return items[atIndex] === toBeChecked;
+}
+var checker = checkElementAt;
+var items = [1, 3, 5, 7];
+var b = checker(items, 5, 1); // false
+// console.log(b); 
+var b2 = checker(items, 5, 2); // true
+var s = { enabled: true, maximized: false };
+// console.log(s);
+// console.log(s['maximized']);
+// Creating a Generic Class
+var GenericNumber = /** @class */ (function () {
+    function GenericNumber() {
+    }
+    return GenericNumber;
+}());
+var myGenericNumber = new GenericNumber();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function (x, y) {
+    return x + y;
+};
+var List = /** @class */ (function () {
+    function List() {
+        this.data = [];
+    }
+    List.prototype.add = function (t) {
+        this.data.push(t);
+    };
+    List.prototype.remove = function (t) {
+        var index = this.data.indexOf(t);
+        if (index > -1) {
+            this.data.splice(index, 1);
+        }
+    };
+    List.prototype.asArray = function () {
+        return this.data;
+    };
+    return List;
+}());
+var numbers = new List();
+numbers.add(11);
+numbers.add(12);
+numbers.add(13);
+numbers.remove(12);
+var numArray = numbers.asArray();
+// console.log(numArray);
+// Using Decorators
+// A Decorator is a special kind of deceleration that can be attached to a
+//    class declaration, method, accessor, property, or parameter.
+// Decorators use the form @expression, where expression must evaluate to a function
+//    that will be called at runtime with information about the decorated declaration.
+// function log(target, key, descriptor) {
+//   console.log(`${key} was called`);
+// }
+// class Calculator {
+//   // using the decorator @log
+//   @log
+//   square(n: number) {
+//     return n * n;
+//   }
+// }
+// Using Arguments in Method Decorator
 function log(target, key, descriptor) {
     // console.log(target);
     var original = descriptor.value;
@@ -12,11 +119,11 @@ function log(target, key, descriptor) {
         for (var _i = 0; _i < arguments.length; _i++) {
             args[_i] = arguments[_i];
         }
-        // Call the original method
+        // call original method
         var result = original.apply(this, args);
-        // Log the call, and the result
-        console.log(key + " with args " + JSON.stringify(args) + " returned\n        " + JSON.stringify(result));
-        // Return the result
+        // Log the call and the result
+        console.log(key + " with args " + JSON.stringify(args) + " returned " + JSON.stringify(result));
+        // return the result
         return result;
     };
     return descriptor;
@@ -24,7 +131,7 @@ function log(target, key, descriptor) {
 var Calculator = /** @class */ (function () {
     function Calculator() {
     }
-    // Using the decorator @log
+    // using the decorator @log
     Calculator.prototype.square = function (n) {
         return n * n;
     };
