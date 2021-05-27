cbb37f2fc31a0487dcd3f3143bd799cb781e1052
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-05-26 18:00:29 -0700

    adds Property Decorator, console log returns as expected

--- typescript-prctice/app.js
+++ typescript-prctice/app.js
@@ -1,101 +1,134 @@
+/////// Generic Function - T Array Type ///////// 
+// function getArray<T>(items: T[]): T[] {
+//   return new Array<T>().concat(items);
+// }
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-// Generic Function - T Array Type
-function getArray(items) {
-    return new Array().concat(items);
-}
-var myNumArr = getArray([100, 200, 300]);
-var myStrArr = getArray(['Hello', 'World']);
-var myBooArr = getArray([true, false]);
+// let myNumArr = getArray([100, 200, 300]);
+// let myStrArr = getArray(['Hello', 'World']);
+// let myBooArr = getArray([true, false]);
 // console.log(myNumArr);
 // console.log(myStrArr); 
 // console.log(myBooArr); 
-// Generic Function - Multiple Type Variables
-function getInfo(id, name) {
-    // console.log((typeof id + ', ' + typeof name));
-}
-getInfo(1, 'Jane'); // number, string
-// Generic Function - Non Generic Type Variables
-function displayType(id, name) {
-    // console.log(typeof id + ', ' + typeof name);
-}
-displayType(2, 'Malik'); // number, string
-// Generic Constraints
-var Customer = /** @class */ (function () {
-    function Customer(fname, lname) {
-        this.firstName = fname;
-        this.lastName = lname;
-    }
-    return Customer;
-}());
-// <T extends Customer> 
-// adds the constraint to limit the customerLogger to taking in only the Customer Type
-function customerLogger(customer) {
-    // console.log(`${customer.firstName} ${customer.lastName}`);
-}
-var customer = new Customer('Jane', 'Doe');
-customerLogger(customer);
-var p = { first: '10k', second: 1000 };
+///////// Generic Function - Multiple Type Variables ///////// 
+// function getInfo<T, U>(id: T, name: U): void {
+//   console.log((typeof id + ', ' + typeof name));
+// }
+// getInfo<number, string>(1, 'Jane'); // number, string
+///////// Generic Function - Non Generic Type Variables ///////// 
+// function displayType<T>(id: T, name: string): void {
+//   console.log(typeof id + ', ' + typeof name);
+// }
+// displayType(2, 'Malik'); // number, string
+///////// Generic Constraints ///////// 
+// class Customer {
+//   firstName: string;
+//   lastName: string;
+//   constructor(fname: string, lname: string) {
+//     this.firstName = fname;
+//     this.lastName = lname;
+//   }
+// }
+//     // <T extends Customer> 
+//     // adds the constraint to limit the customerLogger to taking in only the Customer Type
+// function customerLogger<T extends Customer>(customer: T): void {
+//   console.log(`${customer.firstName} ${customer.lastName}`);
+// }
+// let customer = new Customer('Jane', 'Doe')
+// customerLogger(customer)
+//       // customerLogger('Jane Doe') results in an compiler error
+//       // customerLogger(1) results in an compiler error - type 1 is not an assignable parameter of Customer Type
+///////// Generic Interfaces describing Obj Properties ///////// 
+// An Interface is a group of related properties and methods that describe an object.
+// An Interface does not implement or initialize the object.
+// interface Pair<T, U> {
+//   first: T;
+//   second: U;
+// }
+// let p: Pair<string, number> = { first: '10k', second: 1000 };
 // console.log(p);
-var person = { first: 'Natasha', second: 'Lutzski' };
-var newObj = {
-    id: Math.random().toString(36),
-    run: function () {
-        return 3;
-    }
-};
-function checkElementAt(items, toBeChecked, atIndex) {
-    return items[atIndex] === toBeChecked;
-}
-var checker = checkElementAt;
-var items = [1, 3, 5, 7];
-var b = checker(items, 5, 1); // false
+// const person: Pair<string, string> = {first: 'Natasha', second: 'Lutzski'};
+// console.log(person);
+///////// Interface Command ///////// 
+// interface Command<T, R> {
+//   id: T;
+//   run(): R;
+// }
+// let newObj: Command<string, number> = {
+//   id: Math.random().toString(36), //create a random ID number
+//   run: function() {
+//     return 3;
+//   }
+// };
+// console.log(newObj.id);
+// console.log(newObj.run());
+///////// Define the Function Type inside the Interface ///////// 
+// interface ElementChecker {
+//   <T>(items: T[], toBeChecked: T, atIndex: number): boolean;
+// }
+// function checkElementAt<T>(
+//   items: T[],
+//   toBeChecked: T,
+//   atIndex: number
+// ): boolean {
+//   return items[atIndex] === toBeChecked;
+// }
+// let checker: ElementChecker = checkElementAt;
+// let items = [1, 3, 5, 7];
+// let b: boolean = checker<number>(items, 5, 1); // false
 // console.log(b); 
-var b2 = checker(items, 5, 2); // true
-var s = { enabled: true, maximized: false };
+// let b2: boolean = checker<number>(items, 5, 2); // true
+// console.log(b2); 
+///////// Interface describing indexable ///////// 
+// interface States<R> {
+//   [states: string]: R;
+// }
+// let s: States<boolean> = { enabled: true, maximized: false };
 // console.log(s);
 // console.log(s['maximized']);
-// Creating a Generic Class
-var GenericNumber = /** @class */ (function () {
-    function GenericNumber() {
-    }
-    return GenericNumber;
-}());
-var myGenericNumber = new GenericNumber();
-myGenericNumber.zeroValue = 0;
-myGenericNumber.add = function (x, y) {
-    return x + y;
-};
-var List = /** @class */ (function () {
-    function List() {
-        this.data = [];
-    }
-    List.prototype.add = function (t) {
-        this.data.push(t);
-    };
-    List.prototype.remove = function (t) {
-        var index = this.data.indexOf(t);
-        if (index > -1) {
-            this.data.splice(index, 1);
-        }
-    };
-    List.prototype.asArray = function () {
-        return this.data;
-    };
-    return List;
-}());
-var numbers = new List();
-numbers.add(11);
-numbers.add(12);
-numbers.add(13);
-numbers.remove(12);
-var numArray = numbers.asArray();
+///////// Creating a Generic Class ///////// 
+// class GenericNumber<T> {
+//   zeroValue: T;
+//   add: (x: T, y: T) => T;
+// }
+// let myGenericNumber = new GenericNumber<number>();
+// myGenericNumber.zeroValue = 0;
+// myGenericNumber.add = function(x, y) {
+//   return x + y;
+// };
+///////// Creating a Generic Interface with a class ///////// 
+// interface Collection<T> {
+//   add(t: T): void;
+//   remove(t: T): void;
+//   asArray(): T[];
+// }
+// class List<T> implements Collection<T> {
+//   private data: T[] = [];
+//   add(t: T): void {
+//     this.data.push(t);
+//   }
+//   remove(t: T): void {
+//     let index = this.data.indexOf(t);
+//     if (index > -1) {
+//       this.data.splice(index, 1);
+//     }
+//   }
+//   asArray(): T[] {
+//     return this.data;
+//   }
+// }
+// let numbers: Collection<number> = new List();
+// numbers.add(11);
+// numbers.add(12);
+// numbers.add(13);
+// numbers.remove(12);
+// let numArray = numbers.asArray();
 // console.log(numArray);
-// Using Decorators
+///////// Using Decorators ///////// 
 // A Decorator is a special kind of deceleration that can be attached to a
 //    class declaration, method, accessor, property, or parameter.
 // Decorators use the form @expression, where expression must evaluate to a function
@@ -136,40 +169,69 @@ var numArray = numbers.asArray();
 // calculator.square(2);
 //   //  square with args [3] returned 9
 // calculator.square(3);
-// Configuring Decorators using Decorator Factory
-function log(title) {
-    return function (target, key, descriptor) {
-        // console.log(target);
-        var original = descriptor.value;
-        descriptor.value = function () {
-            var args = [];
-            for (var _i = 0; _i < arguments.length; _i++) {
-                args[_i] = arguments[_i];
-            }
-            // call original method
-            var result = original.apply(this, args);
-            // Log the call and the result
-            console.log("title: " + title + " " + key + " with args " + JSON.stringify(args) + " returned " + JSON.stringify(result));
-            // return the result
-            return result;
-        };
-        return descriptor;
+///////// Configuring Decorators using Decorator Factory ///////// 
+// function log(title: string) {
+//   return function(target, key, descriptor) {
+//     // console.log(target);
+//     const original = descriptor.value;
+//     descriptor.value = function(...args: any[]) {
+//         // call original method
+//       const result = original.apply(this, args);
+//         // Log the call and the result
+//       // console.log(`title: ${title} ${key} with args ${JSON.stringify(args)} returned ${JSON.stringify(result)}`);
+//         // return the result
+//       return result;
+//     };
+//     return descriptor;
+//   }
+// }
+// class Calculator {
+//   // using the decorator Factory Function
+//   @log('Calculator')
+//   square(n: number) {
+//     return n * n;
+//   }
+// }
+// const calculator = new Calculator();
+//   //  square with args [2] returned 4
+// calculator.square(2);
+//   //  square with args [3] returned 9
+// calculator.square(3);
+///////// Using the Property Decorator ///////// 
+function property(target, key) {
+    var value = target[key];
+    // Replacement getter
+    var getter = function () {
+        console.log("Getter for " + key + " returned " + value);
+        return value;
     };
-}
-var Calculator = /** @class */ (function () {
-    function Calculator() {
+    // Replacement setter
+    var setter = function (newVal) {
+        console.log(("Set " + key + " to " + newVal));
+        value = newVal;
+    };
+    // Replace the property
+    var isDeleted = delete this[key];
+    if (isDeleted) {
+        // .defineProperty - adds a property to an obj, or modifies attributes of an existing property
+        Object.defineProperty(target, key, {
+            get: getter,
+            set: setter,
+            enumerable: true,
+            configurable: true
+        });
     }
-    // using the decorator Factory Function
-    Calculator.prototype.square = function (n) {
-        return n * n;
-    };
+}
+var Person = /** @class */ (function () {
+    function Person() {
+    }
     __decorate([
-        log('Calculator')
-    ], Calculator.prototype, "square", null);
-    return Calculator;
+        property
+    ], Person.prototype, "firstName", void 0);
+    return Person;
 }());
-var calculator = new Calculator();
-//  square with args [2] returned 4
-calculator.square(2);
-//  square with args [3] returned 9
-calculator.square(3);
+var person = new Person();
+// set the firstName
+person.firstName = 'Haider';
+// call the getter
+console.log(person.firstName);
--- typescript-prctice/app.ts
+++ typescript-prctice/app.ts
@@ -1,156 +1,156 @@
-// Generic Function - T Array Type
-function getArray<T>(items: T[]): T[] {
-  return new Array<T>().concat(items);
-}
+/////// Generic Function - T Array Type ///////// 
+// function getArray<T>(items: T[]): T[] {
+//   return new Array<T>().concat(items);
+// }
 
-let myNumArr = getArray([100, 200, 300]);
-let myStrArr = getArray(['Hello', 'World']);
-let myBooArr = getArray([true, false]);
+// let myNumArr = getArray([100, 200, 300]);
+// let myStrArr = getArray(['Hello', 'World']);
+// let myBooArr = getArray([true, false]);
 
 // console.log(myNumArr);
 // console.log(myStrArr); 
 // console.log(myBooArr); 
 
-// Generic Function - Multiple Type Variables
-function getInfo<T, U>(id: T, name: U): void {
-  // console.log((typeof id + ', ' + typeof name));
-}
+///////// Generic Function - Multiple Type Variables ///////// 
+// function getInfo<T, U>(id: T, name: U): void {
+//   console.log((typeof id + ', ' + typeof name));
+// }
 
-getInfo<number, string>(1, 'Jane'); // number, string
+// getInfo<number, string>(1, 'Jane'); // number, string
 
-// Generic Function - Non Generic Type Variables
-function displayType<T>(id: T, name: string): void {
-  // console.log(typeof id + ', ' + typeof name);
-}
+///////// Generic Function - Non Generic Type Variables ///////// 
+// function displayType<T>(id: T, name: string): void {
+//   console.log(typeof id + ', ' + typeof name);
+// }
 
-displayType(2, 'Malik'); // number, string
+// displayType(2, 'Malik'); // number, string
 
-// Generic Constraints
-class Customer {
-  firstName: string;
-  lastName: string;
+///////// Generic Constraints ///////// 
+// class Customer {
+//   firstName: string;
+//   lastName: string;
 
-  constructor(fname: string, lname: string) {
-    this.firstName = fname;
-    this.lastName = lname;
-  }
-}
-    // <T extends Customer> 
-    // adds the constraint to limit the customerLogger to taking in only the Customer Type
-function customerLogger<T extends Customer>(customer: T): void {
-  // console.log(`${customer.firstName} ${customer.lastName}`);
-}
+//   constructor(fname: string, lname: string) {
+//     this.firstName = fname;
+//     this.lastName = lname;
+//   }
+// }
+//     // <T extends Customer> 
+//     // adds the constraint to limit the customerLogger to taking in only the Customer Type
+// function customerLogger<T extends Customer>(customer: T): void {
+//   console.log(`${customer.firstName} ${customer.lastName}`);
+// }
 
-let customer = new Customer('Jane', 'Doe')
-customerLogger(customer)
-      // customerLogger('Jane Doe') results in an compiler error
-      // customerLogger(1) results in an compiler error - type 1 is not an assignable parameter of Customer Type
+// let customer = new Customer('Jane', 'Doe')
+// customerLogger(customer)
+//       // customerLogger('Jane Doe') results in an compiler error
+//       // customerLogger(1) results in an compiler error - type 1 is not an assignable parameter of Customer Type
 
-// Generic Interfaces describing Obj Properties
+///////// Generic Interfaces describing Obj Properties ///////// 
   // An Interface is a group of related properties and methods that describe an object.
   // An Interface does not implement or initialize the object.
-interface Pair<T, U> {
-  first: T;
-  second: U;
-}
+// interface Pair<T, U> {
+//   first: T;
+//   second: U;
+// }
 
-let p: Pair<string, number> = { first: '10k', second: 1000 };
+// let p: Pair<string, number> = { first: '10k', second: 1000 };
 // console.log(p);
 
-const person: Pair<string, string> = {first: 'Natasha', second: 'Lutzski'};
+// const person: Pair<string, string> = {first: 'Natasha', second: 'Lutzski'};
 // console.log(person);
 
-// Interface Command
-interface Command<T, R> {
-  id: T;
-  run(): R;
-}
+///////// Interface Command ///////// 
+// interface Command<T, R> {
+//   id: T;
+//   run(): R;
+// }
 
-let newObj: Command<string, number> = {
-  id: Math.random().toString(36), //create a random ID number
-  run: function() {
-    return 3;
-  }
-};
+// let newObj: Command<string, number> = {
+//   id: Math.random().toString(36), //create a random ID number
+//   run: function() {
+//     return 3;
+//   }
+// };
 
 // console.log(newObj.id);
 // console.log(newObj.run());
 
-// Define the Function Type inside the Interface
-interface ElementChecker {
-  <T>(items: T[], toBeChecked: T, atIndex: number): boolean;
-}
+///////// Define the Function Type inside the Interface ///////// 
+// interface ElementChecker {
+//   <T>(items: T[], toBeChecked: T, atIndex: number): boolean;
+// }
 
-function checkElementAt<T>(
-  items: T[],
-  toBeChecked: T,
-  atIndex: number
-): boolean {
-  return items[atIndex] === toBeChecked;
-}
+// function checkElementAt<T>(
+//   items: T[],
+//   toBeChecked: T,
+//   atIndex: number
+// ): boolean {
+//   return items[atIndex] === toBeChecked;
+// }
 
-let checker: ElementChecker = checkElementAt;
-let items = [1, 3, 5, 7];
-let b: boolean = checker<number>(items, 5, 1); // false
+// let checker: ElementChecker = checkElementAt;
+// let items = [1, 3, 5, 7];
+// let b: boolean = checker<number>(items, 5, 1); // false
 // console.log(b); 
-let b2: boolean = checker<number>(items, 5, 2); // true
+// let b2: boolean = checker<number>(items, 5, 2); // true
 // console.log(b2); 
 
-// Interface describing indexable
-interface States<R> {
-  [states: string]: R;
-}
-let s: States<boolean> = { enabled: true, maximized: false };
+///////// Interface describing indexable ///////// 
+// interface States<R> {
+//   [states: string]: R;
+// }
+// let s: States<boolean> = { enabled: true, maximized: false };
 // console.log(s);
 // console.log(s['maximized']);
 
-// Creating a Generic Class
-class GenericNumber<T> {
-  zeroValue: T;
-  add: (x: T, y: T) => T;
-}
+///////// Creating a Generic Class ///////// 
+// class GenericNumber<T> {
+//   zeroValue: T;
+//   add: (x: T, y: T) => T;
+// }
 
-let myGenericNumber = new GenericNumber<number>();
-myGenericNumber.zeroValue = 0;
-myGenericNumber.add = function(x, y) {
-  return x + y;
-};
+// let myGenericNumber = new GenericNumber<number>();
+// myGenericNumber.zeroValue = 0;
+// myGenericNumber.add = function(x, y) {
+//   return x + y;
+// };
 
-// Creating a Generic Interface with a class
-interface Collection<T> {
-  add(t: T): void;
-  remove(t: T): void;
-  asArray(): T[];
-}
+///////// Creating a Generic Interface with a class ///////// 
+// interface Collection<T> {
+//   add(t: T): void;
+//   remove(t: T): void;
+//   asArray(): T[];
+// }
 
-class List<T> implements Collection<T> {
-  private data: T[] = [];
+// class List<T> implements Collection<T> {
+//   private data: T[] = [];
 
-  add(t: T): void {
-    this.data.push(t);
-  }
+//   add(t: T): void {
+//     this.data.push(t);
+//   }
 
-  remove(t: T): void {
-    let index = this.data.indexOf(t);
-    if (index > -1) {
-      this.data.splice(index, 1);
-    }
-  }
+//   remove(t: T): void {
+//     let index = this.data.indexOf(t);
+//     if (index > -1) {
+//       this.data.splice(index, 1);
+//     }
+//   }
 
-  asArray(): T[] {
-    return this.data;
-  }
-}
+//   asArray(): T[] {
+//     return this.data;
+//   }
+// }
 
-let numbers: Collection<number> = new List();
-numbers.add(11);
-numbers.add(12);
-numbers.add(13);
-numbers.remove(12);
-let numArray = numbers.asArray();
+// let numbers: Collection<number> = new List();
+// numbers.add(11);
+// numbers.add(12);
+// numbers.add(13);
+// numbers.remove(12);
+// let numArray = numbers.asArray();
 // console.log(numArray);
 
-// Using Decorators
+///////// Using Decorators ///////// 
 // A Decorator is a special kind of deceleration that can be attached to a
 //    class declaration, method, accessor, property, or parameter.
 // Decorators use the form @expression, where expression must evaluate to a function
@@ -197,33 +197,70 @@ let numArray = numbers.asArray();
 //   //  square with args [3] returned 9
 // calculator.square(3);
 
-// Configuring Decorators using Decorator Factory
-function log(title: string) {
-  return function(target, key, descriptor) {
-    // console.log(target);
-    const original = descriptor.value;
-    descriptor.value = function(...args: any[]) {
-      // call original method
-      const result = original.apply(this, args);
-      // Log the call and the result
-      console.log(`title: ${title} ${key} with args ${JSON.stringify(args)} returned ${JSON.stringify(result)}`);
-      // return the result
-      return result;
-    };
-    return descriptor;
-  }
+///////// Configuring Decorators using Decorator Factory ///////// 
+// function log(title: string) {
+//   return function(target, key, descriptor) {
+//     // console.log(target);
+//     const original = descriptor.value;
+//     descriptor.value = function(...args: any[]) {
+//         // call original method
+//       const result = original.apply(this, args);
+//         // Log the call and the result
+//       // console.log(`title: ${title} ${key} with args ${JSON.stringify(args)} returned ${JSON.stringify(result)}`);
+//         // return the result
+//       return result;
+//     };
+//     return descriptor;
+//   }
   
-}
-class Calculator {
-  // using the decorator Factory Function
-  @log('Calculator')
-  square(n: number) {
-    return n * n;
+// }
+// class Calculator {
+//   // using the decorator Factory Function
+//   @log('Calculator')
+//   square(n: number) {
+//     return n * n;
+//   }
+// }
+
+// const calculator = new Calculator();
+//   //  square with args [2] returned 4
+// calculator.square(2);
+//   //  square with args [3] returned 9
+// calculator.square(3);
+
+///////// Using the Property Decorator ///////// 
+function property(target: any, key: string) {
+  let value = target[key];
+    // Replacement getter
+  const getter = function() {
+    console.log(`Getter for ${key} returned ${value}`);
+    return value
+  };
+   // Replacement setter
+  const setter = function(newVal) {
+    console.log((`Set ${key} to ${newVal}`));
+    value = newVal
+  };
+  // Replace the property
+  const isDeleted = delete this[key];
+  if (isDeleted) {
+    // .defineProperty - adds a property to an obj, or modifies attributes of an existing property
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+      enumerable: true,
+      configurable: true
+    });
   }
 }
 
-const calculator = new Calculator();
-  //  square with args [2] returned 4
-calculator.square(2);
-  //  square with args [3] returned 9
-calculator.square(3);
+class Person {
+  @property
+  public firstName: string;
+}
+const person = new Person();
+  // set the firstName
+person.firstName = 'Haider';
+  // call the getter
+console.log(person.firstName);
+
